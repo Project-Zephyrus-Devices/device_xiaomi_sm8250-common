46bf4da600163762b690cd15809339092795da93
EmanuelCN <emanuelghub@gmail.com>
2022-05-22 20:26:04 +0200

    sm8250-common: Swich to EROFS
    *Requires kernel support.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -149,8 +149,12 @@ TREBLE_PARTITIONS := odm vendor
 ALL_PARTITIONS := $(SSI_PARTITIONS) $(TREBLE_PARTITIONS)
 
 $(foreach p, $(call to-upper, $(ALL_PARTITIONS)), \
-    $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := ext4) \
+    $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := erofs) \
     $(eval TARGET_COPY_OUT_$(p) := $(call to-lower, $(p))))
+
+# EROFS
+BOARD_EROFS_COMPRESSOR := lz4hc,9
+BOARD_EROFS_PCLUSTER_SIZE := 65536
 
 # Partitions - dynamic
 BOARD_SUPER_PARTITION_SIZE := 9126805504
