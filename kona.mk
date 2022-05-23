95c489431f73adf2d5b850e28eed355c1f307b65
chaptsand <chaptsand@gmail.com>
2022-05-23 20:33:10 +0800

    sm8250-common: Build OSS sound trigger HAL
    * Requires to have:
    sound_trigger_mixer_paths.xml
    sound_trigger_platform_info.xml
    
    In device specific tree

--- kona.mk
+++ kona.mk
@@ -169,7 +169,8 @@ PRODUCT_PACKAGES += \
     libsndmonitor \
     libspkrprot \
     libvisualizer \
-    libvolumelistener
+    libvolumelistener \
+    sound_trigger.primary.kona
 
 # Audio configs
 PRODUCT_COPY_FILES += \
