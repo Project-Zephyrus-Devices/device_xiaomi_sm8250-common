9102137aefa216827762e6d2e8d8fe9fe5a655bb
drkphnx <dark.phnx12@gmail.com>
2022-06-09 16:23:21 +0530

    [Squash] "sm8250-common: parts: Drop support for popup camera" * wedon't have it on alioth
    Revert "sm8250-common: Change method used for popup"
    
    This reverts commit 2c86d1045038066ece9ad436a7f120b0b68f1e77.
    
    Revert "sm8250-common: parts: popupcamera: Make sure class attributes are initialized"
    
    This reverts commit 11e64e7c619ba55d2bc097e50ba498db5329e521.
    
    Revert "sm8250-common: parts: popupcamera: Avoid calling mProximitySensor when disabled"
    
    This reverts commit 1f4a7bf2ba4aba4f1540a0d352b6302c806b7aee.
    
    Revert "sm8250-common: parts: Move FOD popup sounds to /system_ext"
    
    This reverts commit dd63e3f87cf0a17765f9bba6cb508f1de691c013.
    
    Revert "sm8250-common: parts: Switch to LED breathing effect"
    
    This reverts commit 4367106b510f15329c8620c337112dfde6acfa8d.
    
    Revert "sm8250-common: parts: Add support for right side LED"
    
    This reverts commit b81d3d51291cbe9d22a3d5fdf6f4445b9bf4334c.
    
    Revert "sm8250-common: parts: Add support for red color"
    
    This reverts commit 801290a1216729274e81b7d75fa208d0c6982192.
    
    Revert "sm8250-common: parts: Check if proximity is near for popupcamera"
    
    This reverts commit 68a54f1016db09f970a141f27701b3aae8c04516.
    
    Revert "sm8250-common: parts: Disable PopupCamera component by default"
    
    This reverts commit ed859470318b3b09a8b697aff9381614eda76161.
    
    Revert "sm8250-common: parts: Refactor popupcamera"
    
    This reverts commit f8dacb27926754e9a6c3a17733968b8eb173dd3e.
    
    Revert "sm8250-common: parts: Add motor calibration"
    
    This reverts commit b3dfce44dd0fcd93e0f2e406aefb7d1b6db935ce.
    
    Revert "sm8250-common: parts: Add popup sound and LED toggle"
    
    This reverts commit a3215df082aafcaff254c88cdc8c56b6629504fd.
    
    Revert "sm8250-common: parts: Add support for camera popup light"
    
    This reverts commit 82275f3fd68b6822a4e9f1379ad03e504ee9a84f.
    
    Revert "sm8250-common: parts: Initial popup camera impl"
    
    This reverts commit 936bf532120c9c58e88db6b2fb13fc4355731eff.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,7 +23,6 @@ import android.content.Intent;
 import android.util.Log;
 
 import org.lineageos.settings.doze.DozeUtils;
-import org.lineageos.settings.popupcamera.PopupCameraUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 
@@ -36,8 +35,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
-        PopupCameraUtils.startService(context);
         ThermalUtils.startService(context);
-        RefreshUtils.startService(context);        
+        RefreshUtils.startService(context);
     }
 }
