42abc6ec0f0fbb80fc549f6e4e74a6d9ae81d5e7
Ziauddin Sameer <ziauddinsameer@gmail.com>
2023-11-09 05:00:59 +0100

    sm8250-comon: remove inclusion of a PRODUCT_BROKEN flag

--- kona.mk
+++ kona.mk
@@ -10,8 +10,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
-PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
